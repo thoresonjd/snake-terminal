ed1474a53d81a761d58d2f480e881fe54a1a1faa
Justin Thoreson <thoresonjd@gmail.com>
2025-11-06 17:21:18 -0800

    Accept grid width and height as command line arguments
    Both the width and height are restricted to a dimension between 5 and 50 inclusive.

--- snake.c
+++ snake.c
@@ -14,11 +14,11 @@
 #include <time.h>
 #include <unistd.h>
 
-static const uint8_t WIDTH = 20, HEIGHT = 20;
-static const uint16_t SIZE = WIDTH * HEIGHT;
+static const uint8_t GRID_DIMENSION_MIN = 5;
+static const uint8_t GRID_DIMENSION_MAX = 50;
 static const char* SNAKE = "\x1b[0;41m ";
 static const char* FOOD = "\x1b[0;46m ";
-static const uint8_t SLEEP_TIME_MILLIS = 50;
+static const uint8_t SLEEP_TIME_MILLIS = 100;
 
 // TODO: use result enum for game state tracking
 #if 0 // currently unused
@@ -42,7 +42,12 @@ typedef struct {
 } coordinate_t;
 
 typedef struct {
-	coordinate_t body[400]; // SIZE 20 * 20
+	uint8_t width;
+	uint8_t height;
+} grid_t;
+
+typedef struct {
+	coordinate_t* body;
 	coordinate_t last_tail;
 	direction_t direction;
 	uint16_t length;
@@ -93,20 +98,21 @@ static uint16_t coord_to_index(coordinate_t coord) {
 #endif
 
 // check for invalid index
-static coordinate_t index_to_coord(uint16_t idx) {
+static coordinate_t index_to_coord(uint16_t idx, const grid_t* const grid) {
 	coordinate_t coord;
-	coord.x = idx % WIDTH;
-	coord.y = idx / WIDTH;
+	coord.x = idx % grid->width;
+	coord.y = idx / grid->width;
 	return coord;
 }
 
-static coordinate_t compute_food(const snake_t* const snake) {
+static coordinate_t compute_food(const snake_t* const snake, const grid_t* const grid) {
 	srand(time(NULL)); // TODO: only call this once at the initialization stage of the program
 	coordinate_t food;
+	uint16_t size = grid->width * grid->height;
 	bool is_cell_available;
 	do {
 		is_cell_available = true;
-		food = index_to_coord(rand() % SIZE);
+		food = index_to_coord(rand() % size, grid);
 		for (uint16_t i = 0; i < snake->length; i++)
 			if (snake->body[i].x == food.x && snake->body[i].y == food.y)
 				is_cell_available = false;
@@ -122,20 +128,22 @@ static void wait() {
 	nanosleep(&req, &rem);	
 }
 
-static snake_t init_snake() {
+static snake_t init_snake(const grid_t* const grid) {
+	uint16_t size = grid->width * grid->height;
 	snake_t snake;
 	snake.length = 1;
-	snake.body[snake.length - 1] = (coordinate_t){ WIDTH / 2, HEIGHT / 2 }; 
+	snake.body = (coordinate_t*)malloc(size * sizeof(coordinate_t));
+	snake.body[snake.length - 1] = (coordinate_t){ grid->width / 2, grid->height / 2 };
 	snake.last_tail = snake.body[snake.length - 1];
 	snake.direction = DIRECTION_RIGHT;
 	return snake;	
 }
 
-static void draw_border() {
-	for (uint8_t i = 0; i < HEIGHT; i++)
-		printf("\x1b[%d;%dH\x1b[0;47m ", i + 1, WIDTH + 1);
-	for (uint8_t i = 0; i <= WIDTH; i++) // account for bottom-right corner
-		printf("\x1b[%d;%dH\x1b[0;47m ", HEIGHT + 1, i + 1);
+static void draw_border(const grid_t* const grid) {
+	for (uint8_t i = 0; i < grid->height; i++)
+		printf("\x1b[%d;%dH\x1b[0;47m ", i + 1, grid->width + 1);
+	for (uint8_t i = 0; i <= grid->width; i++) // account for bottom-right corner
+		printf("\x1b[%d;%dH\x1b[0;47m ", grid->height + 1, i + 1);
 	printf("\x1b[0m");
 }
 
@@ -170,7 +178,7 @@ static void process_input(snake_t* const snake) {
 	}
 }
 
-static void update(snake_t* const snake, coordinate_t* const food) {
+static void update(snake_t* const snake, coordinate_t* const food, const grid_t* const grid) {
 	// shift snake segments
 	snake->last_tail = snake->body[0];
 	for (uint16_t i = 0; i < snake->length - 1; i++)
@@ -180,15 +188,15 @@ static void update(snake_t* const snake, coordinate_t* const food) {
 	direction_t* direction = &snake->direction;
 	if (*direction == DIRECTION_UP && head->y > 0)
 		head->y--;
-	else if (*direction == DIRECTION_DOWN && head->y < HEIGHT - 1)
+	else if (*direction == DIRECTION_DOWN && head->y < grid->height - 1)
 		head->y++;
-	else if (*direction == DIRECTION_RIGHT && head->x < WIDTH - 1)
+	else if (*direction == DIRECTION_RIGHT && head->x < grid->width - 1)
 		head->x++;
 	else if (*direction == DIRECTION_LEFT && head->x > 0)
 		head->x--;
 	// grow snake and create food if snake eats existing one
 	if (head->x == food->x && head->y == food->y) {
-		*food = compute_food(snake);
+		*food = compute_food(snake, grid);
 		snake->length++;
 		snake->body[snake->length - 1] = *head;
 	}
@@ -196,34 +204,42 @@ static void update(snake_t* const snake, coordinate_t* const food) {
 	fflush(stdout);
 }
 
-// TODO: take command line arguments for grid width and height
-#if 0 // currently unused
 static bool parse_uint8(const char* const arg, uint8_t* const value) {
 	if (!arg || !value)
 		return false;
 	int64_t temp;
-	if (!scanf(arg, "%ld", &temp))
+	if (!sscanf(arg, "%ld", &temp))
 		return false;
 	if (temp < 0 || temp > UINT8_MAX)
 		return false;
 	*value = (uint8_t)temp;
 	return true;
 }
-#endif
 
 int main(int argc, char** argv) {
-	snake_t snake = init_snake();
-	coordinate_t food = compute_food(&snake);
+	if (argc != 3)
+		return 1;
+	grid_t grid;
+	if (!parse_uint8(argv[1], &grid.width) || !parse_uint8(argv[2], &grid.height))
+		return 1;
+	if (grid.width < GRID_DIMENSION_MIN || grid.height < GRID_DIMENSION_MIN)
+		return 1;
+	if (grid.width > GRID_DIMENSION_MAX || grid.height > GRID_DIMENSION_MAX)
+		return 1;
+	snake_t snake = init_snake(&grid);
+	coordinate_t food = compute_food(&snake, &grid);
 	struct termios old_terminal = init_terminal();
 	clear_screen();
-	draw_border();
+	draw_border(&grid);
 	do {
 		draw_snake(&snake);
 		draw_food(&food);
 		wait();
 		process_input(&snake);
-		update(&snake, &food);
+		update(&snake, &food, &grid);
 	} while (true); // TODO: terminate loop on game over state
+	free(snake.body);
+	snake.body = NULL;
 	reset_terminal(&old_terminal);
 	return 0;
 }
